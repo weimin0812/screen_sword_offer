0966cf16cd412732841a39d3117d3aa13f400690
shenxiu <weimin@admindeMacBook-Pro.local>
2019-07-21 11:08:48 +0800

    question35

--- src/main/java/offer/question35/Solution.java
+++ src/main/java/offer/question35/Solution.java
@@ -2,36 +2,37 @@ package offer.question35;
 
 public class Solution {
     public int InversePairs(int[] array) {
-        if (array == null || array.length <= 1) {
+        if (array == null | array.length <= 1) {
             return 0;
         }
 
-        return mergeSort(array, 0, array.length - 1);
-    }
-
-    private int mergeSort(int[] array, int l, int h) {
-        if (l < h) {
-            int m = l + (h - l) / 2;
-            int leftCount = mergeSort(array, l, m);
-            int rightCount = mergeSort(array, m + 1, h);
-            int count = merge(array, l, m, h);
-
-            return (leftCount + rightCount + count) % 1000000007;
-
+        int[] t = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            t[i] = array[i];
         }
 
-        return 0;
+        return mergeSort(array, 0, array.length - 1, t);
     }
 
-    private int merge(int[] array, int l, int m, int h) {
-        int[] t = new int[array.length];
+    private int mergeSort(int[] array, int l, int h, int[] t) {
+        if (l >= h) {
+            return 0;
+        }
+        int m = l + (h - l) / 2;
+        int leftCount = mergeSort(array, l, m, t);
+        int rightCount = mergeSort(array, m + 1, h, t);
+        int count = merge(array, l, m, h, t);
+        return (leftCount + count + rightCount) % 1000000007;
+    }
+
+    private int merge(int[] array, int l, int m, int h, int[] t) {
+        int count = 0;
         for (int i = l; i <= h; i++) {
             t[i] = array[i];
         }
 
         int leftIndex = l;
         int rightIndex = m + 1;
-        int count = 0;
         for (int index = l; index <= h; index++) {
             if (leftIndex > m) {
                 array[index] = t[rightIndex++];
@@ -46,6 +47,7 @@ public class Solution {
             }
         }
 
-        return count;
+        return (count % 1000000007);
+
     }
 }
